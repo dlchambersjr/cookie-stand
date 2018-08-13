c6a3ccc938f3ed192152249b1a69b4357b37442e
dlchambersjr <dlchambersjr@gmail.com>
2018-08-13 14:44:40 -0700

    begin js Problem Domain

--- js/app.js
+++ js/app.js
@@ -1,2 +1,26 @@
 'use strict';
 
+// Create object for each location
+
+// Each object will include
+  // minCustomers
+  // maxCustomers
+  // avgCookiesPerCustomer
+  // customersPerHour - random number between min and max customers
+  // cookiesPerHour [] - multiply customersPerHour * avgCookiesPerCustomer and pair with correct hour
+  // totalCookies - sum of all cookies sold in a day
+
+  //method to create a random number of customers between min/max customers
+
+
+//function to pupulate hoursArray
+  // use 24hour for easy math to determine am and pm
+  // var open and close with offsets for populating array - if hour less than 12 use am - else pm
+
+// method to render individual location
+
+
+
+
+
+//for loop to perform the render in an array.
