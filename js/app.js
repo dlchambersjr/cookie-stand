1ea5430c0789b5474fe7a192bfffad2305fe86df
dlchambersjr <dlchambersjr@gmail.com>
2018-08-14 14:18:18 -0700

    Save Point

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@
 // Global use variables
 var allLoc = [], hours = [];
 
+var openHrs = 6, closedHrs = 20;
+
 
 // Constructor fucntion called "Build" to create individual locations
 function Build(name, minCust, maxCust, avgCookies) {
@@ -23,29 +25,25 @@ var seattleCenter = new Build('Seattel Center', 11, 38, 3.7);
 var capitolHill = new Build('Capitol Hill', 20, 38, 2.3);
 var alki = new Build ('Alki', 2, 16, 4.6);
 
-console.log(FirstAndPike);
-console.log(seaTacAirport);
-console.log(seattleCenter);
-console.log(capitolHill);
-console.log(alki);
-console.log(allLoc)
+// Fill hours array based on open and close times using 24hour clock to help determine am/pm
 
-// Fill hours[] based on open and close times
-
-function (openHours() {
-  var open = 6, closed = 20;
-  for (var h = 0; h <= (closed - open) -1 ; h++) {
+function openHours(open,closed) {
+  for (var h = open; h <= closed - 1; h++) {
     
-    if (h + open < 12) {
-      hours[h] = h + open + 'am';
-    } else if ((h + open) === 12) {
-      hours[h] = h + open + 'pm';
+    //Fill Hours Array
+    if (h < 12) {
+      hours.push(h + 'am');
+    } else if (h === 12) {
+      hours.push(h + 'pm');
     } else {
-      hours[h] = h + open - 12 + 'pm';
+      hours.push((h - 12) + 'pm');
     }
+    // Random number of Customers
+
+    // Cookies Sold
+
+    // Daily Totals
   }
-}());
-
-
-console.log(hours)
-
+}
+openHours(openHrs, closedHrs);
+console.log(hours);
